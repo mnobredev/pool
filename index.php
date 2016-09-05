22a49f86d7d124fe7e85f08b6bb25aeb135f29e0
TPSI1015A <TPSI1015A@10.0.2.15>
2016-09-05 22:11:42 +0100

    recovered material

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
-test 
 -->
 <html>
     <head>
@@ -12,8 +11,7 @@ test
     </head>
     <body>
         <?php
-        header("Location: main.php");
+        // put your code here ola
         ?>
-        
     </body>
 </html>
