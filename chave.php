32fc779c6177ad4da693c55796ecf89e35e2f834
Antonio Silva <antnunessilva@gmail.com>
2016-09-09 18:28:59 +0100

    chave added

--- chave.php
+++ chave.php
@@ -1,8 +1,16 @@
 <?php
+$servername="localhost";
+$username="root";
+$password=""; 
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$conn = mysqli_connect($servername, $username, $password);
+
+
+if(!$conn)
+{
+	die("erro".mysqli_connect_error());
+}
+mysqli_select_db($conn, "nome_base_de_dados"); // nome base de dados
+mysqli_set_charset($conn, "utf_8")
+?>
 
