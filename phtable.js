59e765d5ac9e67cb6b94da461afeba0ee8b7b51e
TPSI1015A <TPSI1015A@10.0.2.15>
2016-09-14 22:58:07 +0100

    updated phtable to get a month view

--- phtable.js
+++ phtable.js
@@ -1,110 +1,129 @@
    
     google.charts.load('current', {packages: ['corechart', 'bar']});
-    google.charts.setOnLoadCallback(drawBasic);
+    google.charts.setOnLoadCallback(drawToday);
     
-    google.load('visualization', '1.0', {'packages':['corechart']});
-    google.setOnLoadCallback(drawChart);
-    
-    
-    function drawBasic() {
-       
-        var status="day";
-        var today= new Date();
-        var day = new google.visualization.DataTable();
-        var hour = new google.visualization.DataTable();
+    var status="day";
+    var today= new Date();
+    var selectedDay = today.getDate();
+    var selectedMonth = today.getMonth();
+    var options = {
+            vAxis: {
+                viewWindow: {
+                    min: 6,
+                    max: 8
+                },
+                ticks: [6, 6.5,7,7.5,8]
+            },
+            legend: {position: 'none'}
+        };
+
+    document.getElementById("phback").onclick = function back(){
+        
         var chosenday = new google.visualization.DataTable();
-        var chosenmonth = new google.visualization.DataTable();
-        var selectedDay = today.getDate();
-        var selectedMonth = today.getMonth();
-       
-        document.getElementById("phback").onclick = function back(){
-            
-            if (status=="hour"){
+        if (status=="hour"){
 
-                chosenday.addColumn('string', 'Time of Day');
-                chosenday.addColumn('number', 'PH');
-                var size = dayArray.length;
-        
-                for (var h=0; h<24; h++){
-                    var counter=0;
-                    var media=0;
-                        for (var i=0; i<size; i++){
-                            if (selectedDay==dayArray[i]){
-                        
-                                if (hrArrayphora[i]==h){
-                                    var res = phArrayphora[i].replace(",", ".");
-                                    media+= parseFloat(res);
-                                    counter++;
-                                    console.log(media+" "+h);
-                                }
-                            }
-                    }
-                    if (counter!=0){
-                        chosenday.addRows([[h+':00', media/counter]]);
-                    }
+            chosenday.addColumn('string', 'Time of Day');
+            chosenday.addColumn('number', 'PH');
+            var size = dayArray.length;
+            for (var h=0; h<24; h++){
 
-                    else{
-                        chosenday.addRows([[h+':00', 0]]);
-                    }
-                }
-                chart.draw(chosenday, options);
-                status="day";
-            }
-            else if (status=="day"){
-                chosenmonth.addColumn('number', 'Day of the Month');
-                chosenmonth.addColumn('number', 'PH');
+                var counter=0;
+                var media=0;
 
-                var size = dayArray.length;
-        
-                for (var h=1; h<=31; h++){
-                    var counter=0;
-                    var media=0;
-                        for (var i=0; i<size; i++){
-                            if (selectedMonth+1==9){
-                                if (dayArray[i]==h){
-                                    var res = phArrayphora[i].replace(",", ".");
-                                    media+= parseFloat(res);
-                                    counter++;
-                                    console.log(media+" "+h);
-                                }
-                            }
-                    }
-                    if (counter!=0){
-                        chosenmonth.addRows([[h, media/counter]]);
-                    }
+                    for (var i=0; i<size; i++){
+                        if (selectedDay==dayArray[i] && hrArrayphora[i]==h){
 
-                    else{
-                        chosenmonth.addRows([[h, 0]]);
-                    }
-                }
-                chart.draw(chosenmonth, options);
-                status="month";
-            }
-        }
-        
-        day.addColumn('string', 'Time of Day');
-        day.addColumn('number', 'PH');
-
-        var size = dayArray.length;
-        
-        for (var h=0; h<24; h++){
-            var counter=0;
-            var media=0;
-                for (var i=0; i<size; i++){
-                    if (today.getDate()==dayArray[i]){
-                        
-                        if (hrArrayphora[i]==h){
                             var res = phArrayphora[i].replace(",", ".");
                             media+= parseFloat(res);
                             counter++;
                             console.log(media+" "+h);
                         }
                     }
+
+                if (counter!=0){chosenday.addRows([[h+':00', media/counter]]);}
+
+                else{chosenday.addRows([[h+':00', 0]]);}
+            }
+
+            function selectHandler() {
+
+                var selectedItem = chart.getSelection()[0];
+                    if (selectedItem) {
+                        var value = selectedItem.row;
+                    }
+                clicked(value); 
+            }
+            var formatter = new google.visualization.NumberFormat({fractionDigits: 2});
+            formatter.format(chosenday, 1);
+            var chart = new google.visualization.ColumnChart(document.getElementById('phchart'));
+            google.visualization.events.addListener(chart, 'select', selectHandler);
+            chart.draw(chosenday, options);
+            status="day";
+        }
+
+        else if (status=="day"){
+
+            var chosenmonth = new google.visualization.DataTable();
+            chosenmonth.addColumn('number', 'Day of the Month');
+            chosenmonth.addColumn('number', 'PH');
+            var size = dayArray.length;
+            for (var h=1; h<=31; h++){
+
+                var counter=0;
+                var media=0;
+                for (var i=0; i<size; i++){
+                    if (selectedMonth+1==9){
+                        if (dayArray[i]==h){
+                            var res = phArrayphora[i].replace(",", ".");
+                            media+= parseFloat(res);
+                            counter++;
+                        }
+                    }
+                }
+
+                if (counter!=0){chosenmonth.addRows([[h, media/counter]]);}
+
+                else{chosenmonth.addRows([[h, 0]]);}
+            }
+            
+            function selectHandler() {
+            
+                var selectedItem = chart.getSelection()[0];
+                if (selectedItem) {
+                    var value = selectedItem.row;
+                }
+                clicked(value); 
+            }
+            var formatter = new google.visualization.NumberFormat({fractionDigits: 2});
+            formatter.format(chosenmonth, 1);
+            var chart = new google.visualization.ColumnChart(document.getElementById('phchart'));
+            google.visualization.events.addListener(chart, 'select', selectHandler);
+            chart.draw(chosenmonth,options);
+            status="month";
+        }
+    }
+        
+    function drawToday() {
+        
+        var day = new google.visualization.DataTable();
+        day.addColumn('string', 'Time of Day');
+        day.addColumn('number', 'PH');
+        var size = dayArray.length;
+        
+        for (var h=0; h<24; h++){
+            var counter=0;
+            var media=0;
+            for (var i=0; i<size; i++){
+                if (today.getDate()==dayArray[i] && hrArrayphora[i]==h){
+                    var res = phArrayphora[i].replace(",", ".");
+                    media+= parseFloat(res);
+                    counter++;
+                    console.log(media+" "+h);
+                }
             }
             if (counter!=0){
                 day.addRows([[h+':00', media/counter]]);
             }
-            
             else{
                 day.addRows([[h+':00', 0]]);
             }
@@ -112,17 +131,30 @@
         
         function selectHandler() {
             
-            if (status=="day"){
+            var selectedItem = chart.getSelection()[0];
+            if (selectedItem) {
+                var value = selectedItem.row;
+            }
+            clicked(value); 
+        }
+        var formatter = new google.visualization.NumberFormat({fractionDigits: 2});
+        formatter.format(day, 1);
+        var chart = new google.visualization.ColumnChart(document.getElementById('phchart'));
+        google.visualization.events.addListener(chart, 'select', selectHandler);
+        chart.draw(day, options);
+    }
+    
+    function clicked(value){
+        
+        if (status=="day"){
+           
+            var hour = new google.visualization.DataTable();
+            
             //get by day
             hour.addColumn('string', 'Time of Day');
             hour.addColumn('number', 'PH');
             
-            size = hrArrayphora.length;
-            
-            var selectedItem = chart.getSelection()[0];
-                if (selectedItem) {
-                    var value = selectedItem.row;
-            }
+            var size = hrArrayphora.length;
             
             for (var i=0; i<size; i++){
             
@@ -134,28 +166,54 @@
                 }
                
             }
-            //end get by day
             
+            var formatter = new google.visualization.NumberFormat({fractionDigits: 2});
+            formatter.format(hour, 1);
+            var chart = new google.visualization.ColumnChart(document.getElementById('phchart'));
             chart.draw(hour, options);
             status="hour";
             }
+            
+        if (status=="month"){
+            console.log(value);
+        var day = new google.visualization.DataTable();
+        day.addColumn('string', 'Time of Day');
+        day.addColumn('number', 'PH');
+        var size = dayArray.length;
+        
+        for (var h=0; h<24; h++){
+            var counter=0;
+            var media=0;
+            for (var i=0; i<size; i++){
+                if (value+1==dayArray[i] && hrArrayphora[i]==h){
+                    var res = phArrayphora[i].replace(",", ".");
+                    media+= parseFloat(res);
+                    counter++;
+                    console.log(media+" "+h);
+                }
+            }
+            if (counter!=0){
+                day.addRows([[h+':00', media/counter]]);
+            }
+            else{
+                day.addRows([[h+':00', 0]]);
+            }
         }
-         
+        
+        function selectHandler() {
+            
+            var selectedItem = chart.getSelection()[0];
+            if (selectedItem) {
+                var value = selectedItem.row;
+            }
+            clicked(value); 
+        }
+        status="day";
         var formatter = new google.visualization.NumberFormat({fractionDigits: 2});
         formatter.format(day, 1);
-
-        var options = {
-            vAxis: {
-                viewWindow: {
-                    min: 6,
-                    max: 8
-                },
-                ticks: [6, 6.5,7,7.5,8]
-            },
-            legend: {position: 'none'}
-        };
-
         var chart = new google.visualization.ColumnChart(document.getElementById('phchart'));
         google.visualization.events.addListener(chart, 'select', selectHandler);
         chart.draw(day, options);
+        }
+            
     }
