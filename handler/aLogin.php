2d372ca7b041a3266419dc8baf1f6fd4138f7cb4
iurie Caradja <i.urie@outlook.pt>
2016-09-23 22:42:37 +0100

    aLogin verks

--- handler/aLogin.php
+++ handler/aLogin.php
@@ -8,19 +8,19 @@
 $login = $_REQUEST['email'];
 $pw = $_REQUEST['pass'];
 
-include 'chave.php';
+include '../tools/chave.php';
 
- $sql = mysqli_query($conn, "SELECT h, user_id FROM user WHERE email = '".$login."'");
-  $result ="";
-  $result1 ="";
+    $sql = mysqli_query($conn, "SELECT password, user_id FROM user WHERE email = '".$login."'");
+    $result ="";
+    $result1 ="";
     while($row = mysqli_fetch_array($sql))
     {
-        $result = $row['h'];
+        $result = $row['password'];
         $result1 = $row['user_id']; 
     }
-   $sql1 = mysqli_query($conn, "SELECT first_name, last_name FROM customer WHERE customer_id = '".$result1."'");
-  $result3 ="";
-  $result4 ="";
+    $sql1 = mysqli_query($conn, "SELECT first_name, last_name FROM customer WHERE customer_user_id = '".$result1."'");
+    $result3 ="";
+    $result4 ="";
     while($row1 = mysqli_fetch_array($sql1))
     {
         $result3 = $row1['first_name']; 
