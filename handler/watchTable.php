345472c530486c05fe48c521138e65154402c4df
Mário Nobre <admin@marionobre.com>
2016-10-11 22:55:01 +0100

    index changes

--- handler/watchTable.php
+++ handler/watchTable.php
@@ -1,8 +1,17 @@
 <?php
 include "../tools/chave.php";
- if(isset($_GET["table"])){
-     $table = $_GET["table"];
-     $query="SELECT * from $table";
+if(isset($_GET["table"])){
+    $table = $_GET["table"];
+    $query="SELECT * from $table";
+    if(isset($_GET["where"])){
+        $where = $_GET["where"];
+        $equals = $_GET["equals"];
+        $query="SELECT * from $table where $where=$equals";
+    }
+}
+if(isset($_GET["describe"])){
+    $describe = $_GET["describe"];
+    $query="DESCRIBE $describe";
 }
 
 $sql = mysqli_query($conn, $query);
--- index.php
+++ index.php
@@ -13,7 +13,8 @@ and open the template in the editor.
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="https://www.google.com/jsapi"></script>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <title>Projecto Final ATEC</title>
+        <link rel="stylesheet" href="css/caroussel.css">
+        <title>Aqua Quality Systems</title>
     </head>
     <body style="position: relative;">    
         <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -25,7 +26,7 @@ and open the template in the editor.
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">ATEC - Projecto Final</a>
+                    <a class="navbar-brand" href="#">Aqua Quality Systems</a>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                     <form class="navbar-form navbar-right" method="POST" action="">
@@ -73,17 +74,17 @@ and open the template in the editor.
             </ol>
             <div class="carousel-inner" role="listbox">
                 <div class="item" >
-                    <div class="first-slide" style="min-height: 400px; background-color: #337ab7;"></div>
+                    <div class="first-slide"></div>
                     <div class="container">
                         <div class="carousel-caption">
                             <h1>Já tem o seu equipamento?</h1>
-                            <p>Registe já o seu Arduino e tire partido do sistema de controlo de valores de água mais avançado do mercado.</p>
-                            <p><a class="btn btn-lg btn-default" href="#" role="button">Registe-se já</a></p>
+                            <p>Registe já o seu Aqua e tire partido do sistema de controlo de valores de água mais avançado do mercado.</p>
+                            <p><a class="btn btn-lg btn-default" href="#" role="button" data-toggle="modal" data-target="#newAccount">Registe-se já</a></p>
                         </div>
                     </div>
                 </div>
                 <div class="item">
-                    <div class="second-slide" style="min-height: 400px; background-color: #337ab7;"></div>
+                    <div class="second-slide"></div>
                     <div class="container">
                         <div class="carousel-caption">
                             <h1>Conheça a equipa</h1>
@@ -93,12 +94,12 @@ and open the template in the editor.
                     </div>
                 </div>
                 <div class="item active">
-                    <div class="third-slide" style="min-height: 400px; background-color: #337ab7;"></div>
+                    <div class="third-slide"></div>
                     <div class="container">
                         <div class="carousel-caption">
                             <h1>Fiabilidade e design</h1>
                             <p>O nosso equipamento foi construído para durar e com um design irresistível.</p>
-                            <p><a class="btn btn-lg btn-default" href="#" role="button">Veja a galeria</a></p>
+                            <p><a class="btn btn-lg btn-default" href="#design" role="button">Veja a galeria</a></p>
                         </div>
                     </div>
                 </div>
@@ -114,11 +115,11 @@ and open the template in the editor.
         </div>
         
        
-        <div class="container marketing" style="margin-top: 5%; text-align: center;"> 
+        <div id="design" class="container marketing"> 
             <div class="row featurette">
                 <div class="col-md-7">
                     <h2 class="featurette-heading">Design irresistível <span class="text-muted">Integra-se em qualquer ambiente</span></h2>
-                    <p class="lead">Com um design inspirado em linhas modernas e com acabamentos de luxo o Arduino ocupa muito pouco espaço na sua divisão e dá medições de qualidade de água precisas. A sua interface foi feita a pensar no utilizador comum e permite dar estatísticas que ajudem a prolongar o tempo de vida da sua piscina e a qualidade da água em que mergulha, para uma experiência diferenciadora escolha Arduino.</p>
+                    <p class="lead">Com um design inspirado em linhas modernas e com acabamentos de luxo o seu Aqua ocupa muito pouco espaço na sua divisão e dá medições de qualidade de água precisas. A sua interface foi feita a pensar no utilizador comum e permite dar estatísticas que ajudem a prolongar o tempo de vida da sua piscina e a qualidade da água em que mergulha, para uma experiência diferenciadora escolha Aqua.</p>
                 </div>
                 <div class="col-md-5">
                     <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="img/arduino1.png" data-holder-rendered="true">
@@ -129,29 +130,29 @@ and open the template in the editor.
             
             <div class="row featurette">
                 <div class="col-md-5">
-                    <div id="carousel-example-generic" class="carousel slide featurette-image img-responsive center-block" data-ride="carousel">
+                    <div id="carousel-example-generic"class="carousel slide featurette-image img-responsive center-block" data-ride="carousel">
                         <ol class="carousel-indicators">
                             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                             <li data-target="#carousel-example-generic" data-slide-to="1"></li>
                             <li data-target="#carousel-example-generic" data-slide-to="2"></li>
                         </ol>
                         <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <img src="img/pool1.jpg" alt="img1" style="min-width: 100%;">
-                                <div class="carousel-caption">
-                                PEW PEW PEW
+                            <div class="item active" style="background-color: transparent;">
+                                <img src="img/graphs1.png" alt="img1" style="min-width: 100%;">
+                                <div class="carousel-caption" style="background-color: rgba(1,1,1,0.4); border-radius: 25px;">
+                                    Medições detalhadas e de leitura fácil
                                 </div>
                             </div>
-                            <div class="item">
-                                <img src="img/pool1.jpg" alt="img1" style="min-width: 100%;">
-                                <div class="carousel-caption">
-                                PEW PEW PEW
+                            <div class="item" style="background-color: transparent;">
+                                <img src="img/responsive.png" alt="img1" style="min-width: 100%;">
+                                <div class="carousel-caption" style="background-color: rgba(1,1,1,0.4); border-radius: 25px;">
+                                    A nossa interface adapta-se a qualquer dispositivo.
                                 </div>
                             </div>
-                            <div class="item">
-                                <img src="img/pool1.jpg" alt="img1" style="min-width: 100%;">
-                                <div class="carousel-caption">
-                                PEW PEW PEW
+                            <div class="item" style="background-color: transparent;">
+                                <img src="img/cyber.png" alt="img1" style="min-width: 100%;">
+                                <div class="carousel-caption" style="background-color: rgba(1,1,1,0.4); border-radius: 25px;">
+                                    Encriptamos a sua informação.
                                 </div>
                             </div>
                           </div>
@@ -165,23 +166,10 @@ and open the template in the editor.
     <span class="sr-only">Next</span>
   </a>
 </div>
-                   <!-- <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDUwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzUwMHg1MDAvYXV0bwpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnM+PHN0eWxlIHR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE1NzZkMDkyMDViIHRleHQgeyBmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MjVwdCB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTU3NmQwOTIwNWIiPjxyZWN0IHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIiBmaWxsPSIjRUVFRUVFIi8+PGc+PHRleHQgeD0iMTg1LjEyNSIgeT0iMjYxLjEiPjUwMHg1MDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
-               --> </div>
-                <div class="col-md-7">
-                    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
-            </div>
-            
-            <hr class="featurette-divider">
-            
-            <div class="row featurette">
                 <div class="col-md-7">
-                    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-                </div>
-                <div class="col-md-5">
-                    <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDUwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzUwMHg1MDAvYXV0bwpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnM+PHN0eWxlIHR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE1NzZkMDkyMDViIHRleHQgeyBmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MjVwdCB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTU3NmQwOTIwNWIiPjxyZWN0IHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIiBmaWxsPSIjRUVFRUVFIi8+PGc+PHRleHQgeD0iMTg1LjEyNSIgeT0iMjYxLjEiPjUwMHg1MDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+                    <h2 class="featurette-heading">Experiencia de utilização avançada <span class="text-muted">Uma boa interface não é complicada</span></h2>
+                    <p class="lead">Acreditamos que o utilizador deve ter uma experiência fantástica com as nossas aplicações em que a não seja necessário um manual para usufruir de todas as vantagens do nosso produto, simples mas avançado.</p>
                 </div>
             </div>
             
@@ -196,7 +184,7 @@ and open the template in the editor.
                 <div class="col-lg-4">
                     <img class="img-circle" src="img/asilva.png" alt="Generic placeholder image" width="140" height="140">
                     <h2>António Silva</h2>
-                    <p>Inserir texto genérico aqui!</p>
+                    <p>A programação é um elemento que faz parte do meu dia à dia. A curiosidade levou-me a aprender cada vez mais, e é um enorme prazer fazer parte de um processo de criação tão importante, como o desenvolvimento de software. O Aqua é uma aplicação online, fruto do espirito da nossa equipa, que satisfaz uma necessidade de mercado.</p>
                 </div>
                 <div class="col-lg-4">
                     <img class="img-circle" src="img/fakeiurie.png" alt="Generic placeholder image" width="140" height="140">
